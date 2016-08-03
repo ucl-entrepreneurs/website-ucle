36dda1917096a4daa6b5d2e1677614e1980e2094
arvikachristy <anastasia.arvika@gmail.com>
2016-08-03 23:45:21 +0100

    initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ucle.MvcApplication" Language="C#" %>
---
+++ Javascript/DateTimePicker.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#datetimepicker').datetimepicker({
+        dayOfWeekStart: 1,
+        lang: 'en',
+        disabledDates: ['1986/01/08', '1986/01/09', '1986/01/10'],
+    });
+});
+
